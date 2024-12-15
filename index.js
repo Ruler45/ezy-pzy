a11010d03a4069378fc79f28bba5cb0575796ea0
Sahin <sahin0945@gmail.com>
2024-12-15 12:22:02 +0530

    Basic messaging functionality added

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 import puppeteer from "puppeteer";
 import dotenv from "dotenv";
-
+import generateMessage from "./Message.js";
 dotenv.config();
 
-(async () => {
-  const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_DRIVER_PATH,
-    headless: false,
-  });
+const browser = await puppeteer.launch({
+  executablePath: process.env.CHROME_DRIVER_PATH,
+  headless: false,
+});
 
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1080, height: 760 });
+const page = await browser.newPage();
+await page.setViewport({ width: 1080, height: 640 });
 
-  // Navigate to LinkedIn login page
-  await page.goto("https://linkedin.com/login");
+await page.goto("https://linkedin.com/login");
 
-  // Log in
-  await page.locator("#username").fill(process.env.LINKEDIN_EMAIL);
-  await page.locator("#password").fill(process.env.LINKEDIN_PASSWORD);
+await page.locator("#username").fill(process.env.LINKEDIN_EMAIL);
+await page.locator("#password").fill(process.env.LINKEDIN_PASSWORD);
+// Don't select the "Remember me" checkbox orelse linkedin will flag you as a bot and ask for phone verification
+const checkBtn = await page.$(".remember_me__opt_in");
+if (checkBtn) {
+  await checkBtn.click();
+}
 
-  const checkBtn = await page.$(".remember_me__opt_in");
-  if (checkBtn) {
-    await checkBtn.click();
-  }
+const loginButton = await page.waitForSelector(
+  ".login__form_action_container > button"
+);
+await loginButton.click();
+await loginButton.dispose();
+// await new Promise((resolve) => setTimeout(resolve, 15000));
+await page.waitForNavigation();
 
-  const loginButton = await page.waitForSelector(
-    ".login__form_action_container > button"
-  );
-  await loginButton.click();
-  await page.waitForNavigation();
-
+// Company wise search and connect
+const searchConnect = async (Company) => {
   // Search people
   const searchInput = await page.waitForSelector(
     ".search-global-typeahead__input"
   );
   await searchInput.click();
-  await searchInput.type(""); // Add the term you want to search
+  await searchInput.type(`People at ${Company}`);
   await page.keyboard.press("Enter");
   await page.waitForNavigation();
 
   // Filter for "People"
-  const peopleFilterButton = await page.$$eval(".artdeco-pill", (btns) => {
+  await page.$$eval(".artdeco-pill", (btns) => {
     const btn = btns.find((b) => b.innerText.trim() === "People");
     if (btn) {
       btn.click();
@@ -68,7 +69,7 @@ dotenv.config();
     await new Promise((resolve) => setTimeout(resolve, 2000));
   };
 
-  await filterByCompany("ecell nit silchar");
+  await filterByCompany(Company);
 
   // Click on "Connect" buttons
   const sendInvites = async () => {
@@ -90,8 +91,8 @@ dotenv.config();
           ).filter((btn) => btn.innerText.trim() === "Send")[0];
           if (sendNowButton) {
             sendNowButton.click();
-            await new Promise((resolve) => setTimeout(resolve, 2000));
             results.push("Invite sent.");
+            await new Promise((resolve) => setTimeout(resolve, 2000));
           } else {
             results.push("Send now button not found.");
           }
@@ -110,11 +111,13 @@ dotenv.config();
         if (connectButtons.length > 0) {
           return true;
         } else {
-          const next = document.querySelector(".artdeco-pagination__button--next");
+          const next = document.querySelector(
+            ".artdeco-pagination__button--next"
+          );
           console.log(next);
-          
+
           if (next) {
-            console.log("Next button found.");
+            // console.log("Next button found.");
             next.click();
             await new Promise((resolve) => setTimeout(resolve, 4000));
             return true;
@@ -126,11 +129,99 @@ dotenv.config();
       await new Promise((resolve) => setTimeout(resolve, 2000));
     }
 
-    return resultsSent;
+    return "Invites sent: " + resultsSent.length;
   };
 
   console.log(await sendInvites());
 
   // Close browser (optional for testing)
   // await browser.close();
-})();
+};
+
+const connectionMessage = async (Company) => {
+  // Search people
+  const searchInput = await page.waitForSelector(
+    ".search-global-typeahead__input"
+  );
+  await searchInput.click();
+  await searchInput.type("");
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation();
+
+  // Filter for "People"
+  const peopleFilterButton = await page.$$eval(".artdeco-pill", (btns) => {
+    const btn = btns.find((b) => b.innerText.trim() === "People");
+    if (btn) {
+      btn.click();
+    }
+  });
+  await page.waitForNavigation();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  await page.locator("aria/1st").click();
+  await page.waitForNavigation();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const filterByCompany = async (Company) => {
+    await page.locator("#searchFilter_currentCompany").click();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await page.locator("aria/Add a company").fill(Company);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await page.locator(".basic-typeahead__selectable").click();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await page.locator("aria/Apply current filter to show results").click();
+    await page.waitForNavigation();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+
+  // await filterByCompany(Company);
+
+  const sendMessage = async () => {
+    const resultsSent = [];
+    let hasMore = true;
+
+    await page.exposeFunction("moveMouse", async (x, y) => {
+      // page is accessible here
+
+
+      const name =await page.$eval(".msg-overlay-bubble-header__title",(el)=>el.innerText);
+      const Message = generateMessage(name);
+      await page.locator(".msg-form__contenteditable").click();
+      await page.keyboard.type(Message);
+    });
+    await page.evaluate(async () => {
+      const messageButton = Array.from(
+        document.querySelectorAll("button")
+      ).filter((btn) => btn.innerText.trim() === "Message");
+      messageButton[2].click();
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      window.moveMouse(100, 100);
+      await new Promise((resolve) => setTimeout(resolve, 6000));
+      const sendNowButton = Array.from(
+        document.querySelectorAll("button")
+      ).filter((btn) => btn.innerText.trim() === "Send")[0];
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const closeChatButton = Array.from(document.querySelectorAll(".msg-overlay-bubble-header__control"));
+      const closeBtn=closeChatButton[closeChatButton.length - 1];
+      // console.log(closeChatButton[closeChatButton.length - 1] + " line 202");
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      if (sendNowButton) {
+        sendNowButton.click();
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+        closeBtn.click();
+        return "Message sent.";
+      } else {
+        closeBtn.click();
+        return "Send now button not found.";
+      }
+    });
+  };
+  console.log((await sendMessage()) + " line 207");
+};
+
+// searchConnect("payu");
+
+connectionMessage("Advay NIT Silchar");
